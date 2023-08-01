339cb3b35cffe0bbe59a77740e62c01622946aaa
Kris Taniguchi-Quan <kristinetq@sccwrp.org>
2023-08-01 07:29:38 -0700

    script to tidy csv deliverables

---
+++ PhaseI_Deliverable_Tidy/PhaseI_tidy_csvs.R
@@ -0,0 +1,28 @@
+#'  Cannabis SWB Phase I - Data Deliverable Tidy Script
+#'  
+#'  This script tidy's the original csv deliverables for SFE LOIs (from Paradigm) into combined longer table format
+#'  Integrates file name info into data tables and combines all LOI data into one csv
+#'  
+#'  Subtask deliverables: 
+#'    3.1.2 Natural FFMs for SFE LOIs
+#'    3.1.3.A Flow ecology relationships for key eco management goals
+#'      Flow reduction scenarios
+#'      Type I curves S Coast data
+#'      Type I ecorisk curves S Coast data
+#'      Type II curves
+#'      Type II ecorisk curves
+#'      
+#'@author Kris Taniguchi-Quan, SCCWRP
+#'
+
+
+#data directories (location where csv deliverables are saved)
+ffm_dir <- "C:/Users/kristinet/SCCWRP/Cannabis E-Flows - General/Data/Deliverables_FTP/Phase1_3.1/Phase1_3.1/3.1.2 Geodata LOI Natural FFMs/SFE_LOI_Natural_FFMs/"
+curves_dir <- "C:/Users/kristinet/SCCWRP/Cannabis E-Flows - General/Data/Deliverables_FTP/Phase1_3.1/Phase1_3.1/3.1.3.A Library of Flow-Ecology Curves/"
+
+###############################
+## Tidying 3.1.2 Natural FFMs for SFE LOIs
+
+
+
+
