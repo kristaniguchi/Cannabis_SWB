fa4595625a6d7e280705fc5575742eac5f776b76
Kris Taniguchi-Quan <kristinetq@sccwrp.org>
2025-06-17 16:17:34 -0700

    calculate FFMs using new FFC for impaired flow timeseries from SWB for all LOIs

--- PhaseIII_Impaired_flows_evaluation/Impaired_flows_FFC.R
+++ PhaseIII_Impaired_flows_evaluation/Impaired_flows_FFC.R
@@ -83,8 +83,28 @@ lu_all <- benchmark_lu2 %>%
   #create duplicate column siteID to make sure McBain sites are consistent with flow data
   mutate(siteID_McBain_orig = siteID)
 
-#UPDATE FROM HERE McBain siteIDs: no spaces, remove (Google)
-lu_all$siteID_McBain_orig[lu_all] <- gsub("(Google)", "", lu_all$siteID_McBain_orig)
+#Reformat McBain siteIDs to match impaired flows siteID: remove (Google) and spaces
+ind.google <- grep("(Google)", lu_all$siteID)
+#remove all text inside of parentheses (Google) for mcbainsites
+lu_all$siteID[lu_all$dataset == "mcbainsites"] <- gsub("\\s*\\(.*?\\)", "", lu_all$siteID[lu_all$dataset == "mcbainsites"])
+#remove all spaces in mcbain names
+lu_all$siteID[lu_all$dataset == "mcbainsites"] <- gsub(" ", "", lu_all$siteID[lu_all$dataset == "mcbainsites"])
+#for biosites, all sites starting with "BA_" should be "BA-" to match flow siteID
+lu_all$siteID[lu_all$dataset == "biosites"] <- gsub("^BA-", "BA_", lu_all$siteID[lu_all$dataset == "biosites"])
+
+#find COMIDs that are zero and replace with appropriate COMID
+COMID.0 <- lu_all %>% 
+  filter(COMID == 0)
+#read in LU for NC geomorphic sites to fill in those zeros (only want to do this for 0 COMIDS, since original LU_all has QA'd comids)
+transect_lu <- read_xlsx("C:/Users/kristinet/SCCWRP/Cannabis E-Flows - General/Data/Working/Watershed_Delineation_Tool/Modeled_Flow/Eel_River/Lookup_Tables/LOOKUP_table_all_v2_edits.xlsx", sheet = "LU_transect")
+#filter to siteIDs with COMID zeros
+transect_lu <- transect_lu[transect_lu$siteID %in% COMID.0$siteID,] %>% 
+  select(COMID, siteID)
+#join and replace COMIDs with 0 in lu_all
+lu_all <- lu_all %>% 
+  left_join(transect_lu, by = "siteID", suffix = c("", ".new")) %>%
+  mutate(COMID = coalesce(COMID.new, COMID)) %>%  # Replace if new value exists
+  select(-COMID.new)  # Drop extra column
 
 #FFM metric names etc
 metric.names <- read.csv("C:/Users/kristinet/SCCWRP/Cannabis E-Flows - General/Data/Working/Watershed_Delineation_Tool/Modeled_Flow/Eel_River/Lookup_Tables/all_metric_def_list_FFMs_v2 1.csv")
@@ -94,67 +114,89 @@ metric.names <- read.csv("C:/Users/kristinet/SCCWRP/Cannabis E-Flows - General/D
 
 #create empty df for FFC outputs to be saved
 outputs_ffc <- data.frame()
+#create empty df for warnings for sites that needed to be skipped (couldn't run FFC
+warnings_ffc <- data.frame()
 
 # Set the API endpoint URL
 url <- "https://flowcalculator.codefornature.org/api/calculator"
 
 for(i in 1:length(sites)){
+#for debugging: start from last failed iteration
+#for(i in 285:length(sites)){
+    
   #subset to site i
   flows.SWB.sub <- flows.SWB[flows.SWB$siteID == sites[i],]
   
   #find COMID for site i
-  lu_sitei <- 
+  lu_sitei <- lu_all %>% 
+    filter(dataset == unique(flows.SWB.sub$dataset),
+           siteID == unique(flows.SWB.sub$siteID))
+  #only use first row of lu_sitei (duplicate sites for some NC_final sites)
+  if(length(lu_sitei$COMID)>1){
+    lu_sitei <- lu_sitei[1,]
+  }
   
-  #only use date and flow_cfs_impaired columns
+  #format flow file, only use date and flow_cfs_impaired columns
   flow.file <- flows.SWB.sub %>% 
     select("date", "flow_cfs_impaired") %>% 
     rename("flow" = "flow_cfs_impaired")
+  #write a dummy csv to be replaced each iteration, since it only takes csv file timeseries
+  csv_temp <- "C:/Users/kristinet/OneDrive - SCCWRP/temp_flow_file.csv"
+  write.csv(flow.file, file = csv_temp, row.names = FALSE)
+  
+  # Prepare the CSV file to upload
+  csv_file <- upload_file(csv_temp)
   
   # Define form parameters
   params <- list(
     calculator = "recommended",                # Optional: Choose "reference", "flashy" or "recommended"
     #class = "LSR",                           # Flow class (or use a valid class key)
-    comid = 8212965,                         # Optional COMID if it is available
+    comid = comid.i,                         # Optional COMID if it is available
     file = csv_file                          # The timeseries CSV file
   )
   
+  # Send the POST request with multipart form data
+  response <- POST(url, body = params, encode = "multipart")
   
+  # Parse the JSON response
+  result <- content(response, as = "parsed")
+  # You can continue from here by loading the data into a df, write to disk or whatever your analysis requires!
+  
+  # read the output metrics string from the results and load it into a dataframe
+  df <- read.csv(text = result$output_metrics, header = TRUE)
+  # read metadata to know which calculator was used
+  df_meta <- read.csv(text = result$metadata_file, header = TRUE)
+  
+  #if df is empty there are negative flow values and not enough values in POR for FFC, then skip outputting df and create error message
+  if(length(df$Year) < 1){
+    #print warnings
+    warnings.i <- data.frame(matrix(nrow=1, ncol=1)) %>% 
+      mutate(warnings = result$warnings,
+             iteration = i,
+             siteID = unique(flows.SWB.sub$siteID),
+             dataset = unique(flows.SWB.sub$dataset),
+             COMID = unique(lu_sitei$COMID)) %>% 
+      select(warnings, iteration, siteID, dataset, COMID)
+    
+    # paste into warning output
+    warnings_ffc <- bind_rows(warnings_ffc, warnings.i)
+  }else{
+    # add column with siteID and dataset
+    df <- df %>% 
+      mutate(siteID = unique(flows.SWB.sub$siteID),
+             dataset = unique(flows.SWB.sub$dataset),
+             COMID = unique(lu_sitei$COMID),
+             #output which calculator was used
+             used_calculator = df_meta$Metadata_value[df_meta$Metadata_code == "Used_Calculator"])
+    
+    # paste into output
+    outputs_ffc <- bind_rows(outputs_ffc, df)
+    # Check out the data!
+    # print(head(df))
+  }
 }
 
-#testing new FFC API
-
-# Prepare the CSV file to upload
-csv_file <- upload_file("C:/Users/kristinet/OneDrive - SCCWRP/test_impaired_SFE_2017_209.csv")
-
-# Define form parameters
-params <- list(
-  calculator = "recommended",                # Optional: Choose "reference", "flashy" or "recommended"
-  #class = "LSR",                           # Flow class (or use a valid class key)
-  comid = 8212965,                         # Optional COMID if it is available
-  file = csv_file                          # The timeseries CSV file
-)
-
-# Send the POST request with multipart form data
-response <- POST(url, body = params, encode = "multipart")
-
-# Parse the JSON response
-result <- content(response, as = "parsed")
-# You can continue from here by loading the data into a df, write to disk or whatever your analysis requires!
-
-## Below is an example of reading the flow metrics into a dataframe
-# read the output metrics string from the results and load it into a dataframe
-df <- read.csv(text = result$output_metrics, header = TRUE)
-# Check out the data!
-print(head(df))
+#write csv
+write.csv(outputs_ffc, file="C:/Users/kristinet/SCCWRP/Cannabis E-Flows - General/Data/RawData/NC_impaired_flows_20250515/python_edit_SCCWRP/outputs/FFM_newFFC/Impaired_FFM_NC_LOIs_ALL.csv")
 
 
-
-
-#subset just to test FFC API
-flow.sub <- flows %>% 
-  filter(siteID==sites[1]) %>% 
-  select(date, flow_cfs_impaired) %>% 
-  rename(flow = flow_cfs_impaired) %>% 
-  #reformat date
-  mutate(date = as.POSIXct(date, format = "%m/%d/%Y"))%>% 
-  mutate(date = format(date, "%m/%d/%Y"))
